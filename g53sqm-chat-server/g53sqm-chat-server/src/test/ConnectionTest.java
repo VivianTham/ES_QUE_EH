cbfaf47a859b46597c7919488767f37c45769e4e
OmerN99 <omer.na.ah@gmail.com>
2019-12-21 00:25:36 +0400

    added 11 test cases to ConnectionTest

--- g53sqm-chat-server/g53sqm-chat-server/src/test/ConnectionTest.java
+++ g53sqm-chat-server/g53sqm-chat-server/src/test/ConnectionTest.java
@@ -10,6 +10,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
@@ -25,11 +26,16 @@ class ConnectionTest {
 	private static Socket socket;
 	private static BufferedReader sInput;        // to read from the socket
     private static PrintWriter sOutput;        // to write on the socket
+    
+    static int timeout;
 	
 	@BeforeAll
 	public static void startSocket() throws IOException{
 		new Thread(() -> new Server(9000).startServer()).start();
-		//creating socket
+	}
+	
+	@BeforeEach
+	public void initialiseClient() {
 		try {
 			Thread.sleep(100);
             socket = new Socket("localhost", 9000);
@@ -41,6 +47,10 @@ class ConnectionTest {
         }	
 	}
 	
+	@AfterEach
+	public void quitClient() {
+		sOutput.println("QUIT");
+	}
 	
 	@Test
 	@Order(1)
@@ -53,23 +63,153 @@ class ConnectionTest {
 	@Test
 	@Order(2)
 	public void should_NotifyError_When_InvalidCommand() throws IOException{
+		sInput.readLine();
 		String input = "MSG"; //command length < 4
-		PrintWriter i = getPrintWriter();
-		sendCommand(input, i);
+		sOutput.println(input);
 		String msg = sInput.readLine();
 		assertEquals("BAD invalid command to server",msg);
 	}
 	
-	public void sendCommand(String cmd, PrintWriter i) {
-		i.println(cmd);
-		i.flush();
+	@Test
+	@Order(3)
+	public void testListMethod_WithoutRegisteredUser() throws IOException{
+		sInput.readLine();
+		String input = "LIST";
+		sOutput.println(input);
+		String msg = sInput.readLine();
+		assertEquals("BAD You have not logged in yet",msg);
 	}
 	
-	public PrintWriter getPrintWriter() throws IOException{
-		OutputStream os = socket.getOutputStream();
-        OutputStreamWriter osw = new OutputStreamWriter(os);
-        PrintWriter writer = new PrintWriter(osw);
-
-        return writer;
+	@Test
+	@Order(4)
+	public void testListMethod_WithRegisteredUser() throws Exception{
+		sInput.readLine();
+		String username = "testUser";
+		sOutput.println("IDEN " + username);
+		sInput.readLine();
+		Thread.sleep(timeout);
+		
+		sOutput.println("LIST");
+		Thread.sleep(timeout);
+		
+		
+		String msg = sInput.readLine();
+		assertEquals("OK " + username + ", ", msg);
+		
+		sOutput.println("QUIT");
+	}
+	
+	@Test
+	@Order(5)
+	public void testIdenMethod_NewRegistration() throws Exception{
+		//initialiseClient();
+		sInput.readLine();
+		
+		String username = "testUser";
+		sOutput.println("IDEN " + username);
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("OK Welcome to the chat server " + username, msg);
+		
+		sOutput.println("QUIT");
+	}
+	
+	@Test
+	@Order(6)
+	public void testIdenMethod_AlreadyRegistered() throws Exception{
+		//initialiseClient();
+		sInput.readLine();
+		
+		String username = "testUser";
+		sOutput.println("IDEN " + username);
+		Thread.sleep(timeout);
+		sInput.readLine();
+		
+		String username1 = "testUser1";
+		sOutput.println("IDEN " + username1);
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("BAD you are already registerd with username " + username, msg);
+		
+		sOutput.println("QUIT");
+	}
+	
+	@Test
+	@Order(7)
+	public void testStatMethod_UNREGISTERED() throws Exception{
+		//initialiseClient();
+		sInput.readLine();
+		
+		sOutput.println("STAT");
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("OK You have not logged in yet", msg);
+	}
+	
+	@Test
+	@Order(8)
+	public void testStatMethod_REGISTERED() throws Exception{
+		sInput.readLine();
+		
+		String username = "testUser";
+		sOutput.println("IDEN " + username);
+		Thread.sleep(timeout);
+		sInput.readLine();
+		
+		sOutput.println("STAT");
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("You are logged im and have sent 0 message(s)", msg);
+		
+		sOutput.println("QUIT");
+	}
+	
+	@Test
+	@Order(9)
+	public void testMesgMethod_UNREGISTERED() throws Exception{
+		//initialiseClient();
+		sInput.readLine();
+		
+		sOutput.println("MESG Omer Hello!");
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("BAD You have not logged in yet", msg);
+	}
+	
+	@Test
+	@Order(10)
+	public void testHailMethod_UNREGISTERED() throws Exception{
+		sInput.readLine();
+		
+		sOutput.println("HAIL Hellooo!");
+		Thread.sleep(timeout);
+		
+		String msg = sInput.readLine();
+		assertEquals("BAD You have not logged in yet", msg);
+	}
+	
+	@Test
+	@Order(11)
+	public void testHailMethod_REGISTERED() throws Exception{
+		sInput.readLine();
+		
+		String username = "testUser";
+		sOutput.println("IDEN " + username);
+		Thread.sleep(timeout);
+		sInput.readLine();
+		
+		String broadcast = "Hellooo!";
+		sOutput.println("HAIL " + broadcast);
+		
+		String msg = sInput.readLine();
+		assertEquals("Broadcast from " + username + ": " + broadcast, msg);
+		
+		sOutput.println("QUIT");
+		
 	}
 }
