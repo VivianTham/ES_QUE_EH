e95df981c0fa61981e6d9850abafa92bbe922aed
weiweikimberly <38785632+weiweikimberly@users.noreply.github.com>
2019-12-21 04:59:12 +0800

    added test cases for ServerTest

--- g53sqm-chat-server/g53sqm-chat-server/src/test/ServerTest.java
+++ g53sqm-chat-server/g53sqm-chat-server/src/test/ServerTest.java
@@ -7,10 +7,10 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-
-
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -22,6 +22,8 @@ import org.junit.jupiter.api.Test;
 
 import g53sqm.chat.server.Server;
 
+import org.hamcrest.collection.IsEmptyCollection;
+
 class ServerTest{
 	
 	static int timeout = 100;
@@ -30,6 +32,18 @@ class ServerTest{
 	private static BufferedReader sInput;        // to read from the socket
     private static PrintWriter sOutput;        // to write on the socket
     private static Server server = new Server(9000);
+    
+    public void initialiseClient() {
+		try {
+			Thread.sleep(100);
+            socket = new Socket("localhost", 9000);
+            
+            sInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sOutput = new PrintWriter(socket.getOutputStream(), true);
+        } catch (Exception ec) {      // exception handler if it failed
+            System.out.println("Error connecting to server:" + ec);
+        }	
+	}
     
   
 	@BeforeAll
@@ -48,6 +62,14 @@ class ServerTest{
         }	
 	}
 	
+	@Test
+	void Should_ReturnUserList() {
+		ArrayList<String> userList = server.getUserList();
+		
+		assertThat(actual, not(IsEmptyCollection.empty()));
+
+	    assertThat(new ArrayList<>(), IsEmptyCollection.empty());
+	}
 
 	@Test
 	void Should_ReturnFalse_When_UserDoesNotExist(){
@@ -75,6 +97,35 @@ class ServerTest{
 		assertTrue(existence);
 		
 		sOutput.println("QUIT");
-		
 	}
+	
+
+	@Test
+	void Should_ReturnNumberOfUsers_When_ListNotEmpty() throws Exception {
+		initialiseClient();
+		//given
+		//login user
+		String userName = "Omer";
+		sOutput.println("IDEN " + userName);
+		Thread.sleep(timeout);
+		
+		//when
+		int userCount = server.getNumberOfUsers();
+		
+		//then
+		assertEquals(1, userCount);
+		sOutput.println("QUIT");
+	}	
+	
+	
+	@Test
+	void Should_ReturnZero_When_ListEmpty() throws Exception {
+		initialiseClient();
+		
+		//when
+		int userCount = server.getNumberOfUsers();
+	
+		//then
+		assertEquals(0, userCount);
+	}	
 }
